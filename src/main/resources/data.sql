4194f87d0bf8bff70be05782c2518d4ca49c8b40
Tus <54474594+tuskingcup@users.noreply.github.com>
2023-11-12 22:59:30 +0700

    config data sorce

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
 -- Insert data into the Tag table
 use lightcode;
-INSERT INTO Tag (topic, description) VALUES
-                                         ('Java', 'Java programming language'),
-                                         ('Spring', 'Spring Framework'),
-                                         ('SQL', 'Structured Query Language')
+INSERT INTO Tag (id, topic, description) VALUES
+                                         (1, 'Java', 'Java programming language'),
+                                         (2, 'Spring', 'Spring Framework'),
+                                         (3, 'SQL', 'Structured Query Language')
 -- add more rows as needed
 ;
 
 -- Insert data into the Lesson table
-INSERT INTO Lesson (tag_id, name, content) VALUES
-                                               (1, 'Introduction to Java', 'This lesson covers the basics of Java programming.'),
-                                               (1, 'Object-Oriented Programming', 'Learn about OOP concepts in Java.'),
-                                               (2, 'Spring Boot Overview', 'Introduction to Spring Boot framework.'),
-                                               (2, 'Dependency Injection', 'Understanding the concept of dependency injection in Spring.'),
-                                               (3, 'SQL Basics', 'Introduction to basic SQL commands.')
+INSERT INTO Lesson (id, tag_id, name, content) VALUES
+                                               (1, 1, 'Introduction to Java', 'This lesson covers the basics of Java programming.'),
+                                               (2, 1, 'Object-Oriented Programming', 'Learn about OOP concepts in Java.'),
+                                               (3, 2, 'Spring Boot Overview', 'Introduction to Spring Boot framework.'),
+                                               (4, 2, 'Dependency Injection', 'Understanding the concept of dependency injection in Spring.'),
+                                               (5, 3, 'SQL Basics', 'Introduction to basic SQL commands.')
 ;
