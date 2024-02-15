47ffcbb5a4724f575a1bfc502b800eb527948937
Amornpong Duangchawee <amornpong.213@gmail.com>
2024-02-15 20:40:40 +0700

    cors fixed

--- src/main/java/com/senior/dreamteam/authentication/SecurityConfig.java
+++ src/main/java/com/senior/dreamteam/authentication/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.addAllowedOriginPattern("*");
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*")); // Allow all headers
         configuration.setAllowCredentials(true);
