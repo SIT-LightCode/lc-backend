23cba7e05d2562f6b1a3c22f8e6820019b18a52e
Amornpong Duangchawee <amornpong.213@gmail.com>
2024-02-15 17:42:03 +0700

    .

---
+++ src/main/java/com/senior/dreamteam/configs/GenericExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.senior.dreamteam.configs;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@NoArgsConstructor
+@Slf4j
+@ControllerAdvice
+public class GenericExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .findFirst()
+                .orElse("Validation error");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        String errorMessage = ex.getReason();
+        // If there's no custom message, use a default one
+        if (errorMessage == null) {
+            errorMessage = "There was an error processing your request.";
+        }
+        return ResponseEntity.status(ex.getStatusCode()).body(errorMessage);
+    }
+
+}
