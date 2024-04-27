ed90946e6f55889bdd86ba3f5544dc22bcf35bfa
Huaat <amornpong.213@gmail.com>
2024-04-27 23:25:10 +0700

    x

--- src/main/java/com/senior/dreamteam/entities/TagInput.java
+++ src/main/java/com/senior/dreamteam/entities/TagInput.java
@@ -1,7 +1,10 @@
 package com.senior.dreamteam.entities;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+import org.springframework.graphql.data.method.annotation.Argument;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,6 +15,7 @@ public class TagInput {
     int id;
 
     @NotEmpty(message = "topic must not be empty")
+    @Min(value = 0, message = "topic length must be greater than or equal to 0") @Max(value = 5, message = "topic length must be lesser than or equal to 255")
     String topic;
     @NotEmpty(message = "description must not be empty")
     String description;
