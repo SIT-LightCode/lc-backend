7d13a3c91ad8a57f3303c036613b44d1dcb04cd3
Huaat <amornpong.213@gmail.com>
2024-05-19 23:18:39 +0700

    password

--- src/main/java/com/senior/dreamteam/authentication/payload/ChangePasswordRequest.java
+++ src/main/java/com/senior/dreamteam/authentication/payload/ChangePasswordRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
 
 public record ChangePasswordRequest(
         @NotEmpty(message = "Email cannot be empty")
@@ -11,7 +12,7 @@ public record ChangePasswordRequest(
         String email,
         @NotEmpty(message = "Password cannot be empty")
         String password,
-        @NotEmpty(message = "New password cannot be empty") @Min(value = 6, message = "Password length must more than 6") @Max(value = 20, message = "Password length must less than 20")
+        @NotEmpty(message = "New password cannot be empty") @Length(min = 6, message = "Password length must more than 6") @Length(max = 20, message = "Password length must less than 20")
         String newPassword
 ) {
 }
